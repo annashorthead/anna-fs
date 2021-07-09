1a1ffab2580a70cf48669824caa28d018da4abf7
Anna Voice <aqprodigy@gmail.com>
2021-07-09 18:47:22 +0500

    created main window, working

--- app.js
+++ app.js
@@ -4,12 +4,27 @@ const path = require('path');
 //const fs = require('ray-fs');
 const src = "src";
 const UI = path.join(src, "UI");
+let mainWin;
+const theme = {
+  backgroundColor:"#00f",
+  maxWidth: 1200,
+  maxHeight: 800,
+  frame: false,
+  minWidth: 300,
+  minHeight: 200,
+}
 
 function whenReady(){
   const mainWinUI = path.join("file://", __dirname, UI, "mainWin.html");
-  const mainWin = new BrowserWindow();
+  mainWin = new BrowserWindow(theme);
   mainWin.webContents.loadURL(mainWinUI);
   console.log("app is ready");
 }
 
+function whenClosed(){
+  mainWin = null;
+}
+
 app.on("ready", whenReady);
+
+app.on("closed", whenClosed);
